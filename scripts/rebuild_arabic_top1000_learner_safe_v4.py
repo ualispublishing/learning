8a844516ac37033a2548bb9129ec835f30dbd651
ualispublishing <ualispublishing@icloud.com>
2026-08-11 19:04:40 -0400

    Correct final Arabic malformed demonstrative spelling

--- scripts/rebuild_arabic_top1000_learner_safe_v4.py
+++ scripts/rebuild_arabic_top1000_learner_safe_v4.py
@@ -7,6 +7,7 @@ import rebuild_arabic_top1000_learner_safe_v3 as v3
 v3.v2.final.FINAL_REPAIRS.update({
     209: "نسوة",
     292: "ألا",
+    353: "هؤلاء",
     389: "إسلامي",
 })
 
@@ -15,6 +16,7 @@ v3.v2.MANUAL.update({
     209: "women; group of women",
     270: "type; kind; form",
     292: "attention/emphasis particle; also unvocalized form of أَلَّا 'that not'",
+    353: "these",
     389: "Islamic",
     500: "speech; statement; remark; talk; saying",
     550: "results; consequences",
@@ -32,6 +34,7 @@ v3.v2.POS_OVERRIDE.update({
     209: "collective noun / plural noun",
     270: "noun",
     292: "particle",
+    353: "demonstrative pronoun",
     389: "adjective",
     500: "noun",
     550: "noun",
